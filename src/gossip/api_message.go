a19e46fb5c3e4cb733b6a9721170a2021c3d1854
Deniz Etkar <denizetkar@gmail.com>
2020-09-02 00:14:29 +0200

    added gossip round logic from Karp et al. 'median-counter algorithm'; added incomingGossips field into the Gossiper for storing incoming gossips until next round; added nextRoundPullPeers field into the Gossiper for storing the random peers provided by the Central controller to make pull request on the next round; updated the string representation of the Gossiper; added Who field into the GossipNotificationMSGPayload for specifying which client to notify

---
+++ src/gossip/api_message.go
@@ -0,0 +1,24 @@
+package main
+
+// APIListenerCrashedMSGPayload is the payload type of an InternalMessage
+// with type APIListenerCrashedMSG.
+type APIListenerCrashedMSGPayload error
+
+// APIListenerClosedMSGPayload is the payload type of an InternalMessage
+// with type APIListenerClosedMSG.
+type APIListenerClosedMSGPayload void
+
+// APIEndpointCrashedMSGPayload is the payload type of an InternalMessage
+// with type APIEndpointCrashedMSG.
+type APIEndpointCrashedMSGPayload struct {
+	endp     *APIEndpoint
+	isReader bool
+	err      error
+}
+
+// APIEndpointClosedMSGPayload is the payload type of an InternalMessage
+// with type APIEndpointClosedMSG.
+type APIEndpointClosedMSGPayload struct {
+	endp     *APIEndpoint
+	isReader bool
+}
