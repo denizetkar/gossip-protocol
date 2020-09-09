37f29837af4efe9b08e7d1777e6228395e5d1449
Daniel Fomin <ga63ros@mytum.de>
2020-09-10 00:33:57 +0200

    Proof Of Work

--- src/crypto/securecomm/identity_proof.go
+++ src/crypto/securecomm/identity_proof.go
@@ -2,6 +2,8 @@ package securecomm
 
 import (
 	"github.com/monnand/dhkx"
+	"golang.org/x/crypto/scrypt"
+	"math/rand"
 )
 
 type KeyManagement struct {
@@ -30,4 +32,22 @@ func (km *KeyManagement) computeExtDHKey(b []byte) []byte {
 	// Compute the key
 	k, _ := km.dh_group.ComputeKey(pubKey, km.dh_priv)
 	return k.Bytes()
+}
+
+func proofOfWork(k int, pre_m []byte) ([]byte, error) {
+	nonce := make([]byte, 64)
+	rand.Read(nonce)
+	m := append(pre_m, nonce...)
+	// https://wizardforcel.gitbooks.io/practical-cryptography-for-developers-book/content/mac-and-key-derivation/scrypt.html
+	// Memory required = 128 * N * r * p bytes
+	hash, err := scrypt.Key(m, nonce, 16384, 8, 1, 128)
+	if err != nil {
+		return nil, err
+	}
+	for i := 0; i < k; i++ {
+		if hash[i] != 0 {
+			return proofOfWork(k, pre_m)
+		}
+	}
+	return m, nil
 }
